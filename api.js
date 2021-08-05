5ffa25dd0e5738487c6ab18f62c1cf2424294e31
Esther <eagyare91@gmail.com>
2021-08-05 23:19:20 +0100

    removing api key

--- api.js
+++ api.js
@@ -64,37 +64,7 @@ function handleSubmit(event) {
 
 // Google Maps
 
-// const googleMapUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyD4IYua0BKbAxiGQwZ5efvwb3NNJnJ1c40&callback=initMap";
 
-
-
-// function initMap() {
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: 3,
-//     center: { lat: -28.024, lng: 140.887 },
-//   });
-//   // Create an array of alphabetical characters used to label the markers.
-//   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   // Add some markers to the map.
-//   // Note: The code uses the JavaScript Array.prototype.map() method to
-//   // create an array of markers based on a given "locations" array.
-//   // The map() method here has nothing to do with the Google Maps API.
-//   const markers = locations.map((location, i) => {
-//     return new google.maps.Marker({
-//       position: location,
-//       label: labels[i % labels.length],
-//     });
-//   });
-//   // Add a marker clusterer to manage the markers.
-//   new MarkerClusterer(map, markers, {
-//     imagePath:
-//       "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-//   });
-// }
-
-// var map = new google.maps.Map(document.getElementById("map"),mapProp);
-
-// //<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD4IYua0BKbAxiGQwZ5efvwb3NNJnJ1c40&callback=initMap"></script>
 
 
 
